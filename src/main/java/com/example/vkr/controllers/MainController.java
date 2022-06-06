3421ea8ab807413e4d94bc3d327384e8389889f4
prettygodboi <dima.atzo@gmail.com>
2022-06-06 19:14:35 +0500

    commit

--- src/main/java/com/example/vkr/controllers/MainController.java
+++ src/main/java/com/example/vkr/controllers/MainController.java
@@ -1,7 +1,10 @@
 package com.example.vkr.controllers;
 
 import com.example.vkr.entities.Product;
+import com.example.vkr.services.OrderService;
 import com.example.vkr.services.ProductService;
+import com.example.vkr.services.RoleService;
+import com.example.vkr.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +17,28 @@ import java.util.Optional;
 public class MainController {
 
     private final ProductService productService;
+    private final OrderService orderService;
+    private final UserService userService;
+    private final RoleService roleService;
 
     @GetMapping("/addProduct")
     public void createProduct(){
         Product product = Product.builder().name("Шлёпка").build();
         productService.save(product);
     }
+    @GetMapping("/createUser")
+    public void createUser(){
+
+    }
     @GetMapping("/deleteProduct/{id}")
-    public void deleteProduct(@PathVariable(value = "id") Long id){
+    public String deleteProduct(@PathVariable(value = "id") Long id){
         productService.deleteById(id);
+        return "";
+    }
+    @GetMapping("/deleteOrder/{id}")
+    public String deleteOrder(@PathVariable(value = "id") Long id){
+        orderService.deleteById(id);
+        return "";
     }
 
 }
